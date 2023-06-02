7ca9b7ebf8c677e9e0dabacf04482aa0ce990cfd
kuromika <kuromika.dev@gmail.com>
2023-06-02 16:30:58 -0500

    Refactor components using objects - 1.3

--- course-information/src/App.js
+++ course-information/src/App.js
@@ -7,15 +7,15 @@ const Content = (props) => {
 
   return (
     <div>
-      <Part name={props.part1} exercises={props.exercises1}></Part>
-      <Part name={props.part2} exercises={props.exercises2}></Part>
-      <Part name={props.part3} exercises={props.exercises3}></Part>
+      <Part part={props.part1}></Part>
+      <Part part={props.part2}></Part>
+      <Part part={props.part3}></Part>
     </div>
   )
 }
 
 const Part = (props) => {
-  return (<p>{props.name} {props.exercises}</p>)
+  return (<p>{props.part.name} {props.part.exercises}</p>)
 }
 
 const Total = (props) => {
@@ -26,18 +26,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course}></Header>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}></Content>
-      <Total total={exercises1 + exercises2 + exercises3}></Total>
+      <Content part1={part1} part2={part2} part3={part3}></Content>
+      <Total total={part1.exercises + part2.exercises + part3.exercises}></Total>
     </div>
   )
 }
