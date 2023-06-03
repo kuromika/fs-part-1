34818b748ca6007b04fe396a7a64bd549609fe13
kuromika <kuromika.dev@gmail.com>
2023-06-02 20:10:16 -0500

    Display statistics in table - 1.11

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -17,26 +17,31 @@ const Feedback = ({increaseGood, increaseNeutral, increaseBad}) => {
 }
 
 const StatisticLine = ({ text, value }) => {
-  return <p>{text} {value}</p>
+  return(
+  <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+  </tr>)
 }
 
 const Statistics = ({ good, neutral, bad }) => {
 
   const all = good + neutral + bad;
 
-
   return (
     <div>
       <h1>Statistics</h1>
       {all > 0 ?
-        <>
-      <StatisticLine text='good' value={good}></StatisticLine>
-      <StatisticLine text='neutral' value={neutral}></StatisticLine>
-      <StatisticLine text='bad' value={bad}></StatisticLine>
-      <StatisticLine text='all' value={all}></StatisticLine>
-      <StatisticLine text='average' value={(good - bad) / all}></StatisticLine>
-      <StatisticLine text='positive' value={ (good / all) * 100}></StatisticLine>
-        </> : <p>No feedback given</p>
+        <table>
+          <tbody>
+            <StatisticLine text='good' value={good}></StatisticLine>
+            <StatisticLine text='neutral' value={neutral}></StatisticLine>
+            <StatisticLine text='bad' value={bad}></StatisticLine>
+            <StatisticLine text='all' value={all}></StatisticLine>
+            <StatisticLine text='average' value={(good - bad) / all}></StatisticLine>
+            <StatisticLine text='positive' value={`${(good / all) * 100} %`}></StatisticLine>
+          </tbody>
+        </table> : <p>No feedback given</p>
       }
     </div>
   )
